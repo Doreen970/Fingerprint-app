3f4d117d94b937df6e6af4b38c39e0bb20550bfc
Harrison Itotia <harrisonitotia@gmail.com>
2024-06-08 11:57:17 +0300

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
-import { BrowserRouter as Router, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Login from './components/Login'
 
 
 function App() {
@@ -7,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        
+      <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   )
